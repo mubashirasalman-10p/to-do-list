48cf025a0c3177720f36ec689a67480e394cf500
mubashirasalman-10p <mubashira.salman@tenpearls.com>
2021-01-22 11:42:29 +0500

    added card component

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ class AddNew extends React.Component{
             name="text"
             autoComplete="off"
             />
-            <button type="submit" className="btn btn__primary btn__lg">
+            <button type="submit" className="btn">
             Add
             </button>
         </form>
@@ -22,11 +22,30 @@ class AddNew extends React.Component{
     }
 }
 
+class TaskList extends React.Component{
+    render(){
+        return(
+            <div id="task-list">
+                <div class="card text-center" style={{width: "18rem"}}>
+                    <div class="card-body">
+                        <h5>Today's Tasks:</h5>
+                        <ul>
+                            <li>Task One</li>
+                            <li>Task Two</li>
+                        </ul>
+                        <br></br>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
 class ItemCount extends React.Component {
     render() {
       return (
-          <div>
-              <h1 style={{color:"blue", textAlign:"center"}}>Total Items: {this.props.totalItems}</h1>
+          <div id="item-count">
+              <h1 style={{color:"blue", textAlign:"center"}}>Total Tasks: {this.props.totalItems}</h1>
           </div>
       );
     }
@@ -37,6 +56,7 @@ class ItemCount extends React.Component {
           return(
               <div>
               <AddNew/>
+              <TaskList/>
               <ItemCount totalItems={0}/>
               </div>
           );
